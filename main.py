7490e8dbd3019ad463efbef00c68d609a7ea1fb1
oscar-cbrlc <oscar.cbrlc@gmail.com>
2026-04-30 01:20:50 -0600

    add api key check

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status, Security
+from fastapi.security.api_key import APIKeyHeader
+
 from sqlalchemy.orm import Session
 
 from database import SessionLocal, engine, Base
@@ -18,6 +20,8 @@ load_dotenv('.env')
 #create tables if they don't exist
 Base.metadata.create_all(bind=engine)
 
+is_production = os.getenv("ENVIRONMENT") == "production"
+
 def get_db():
     db = SessionLocal()
     try:
@@ -27,7 +31,19 @@ def get_db():
 
 app = FastAPI()
 
-@app.post("/readings", response_model=ReadingResponse)
+API_KEY = os.getenv("API_KEY")
+API_KEY_NAME = "X-API-Key"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+async def get_api_key(api_key: str = Security(api_key_header)):
+    if api_key == API_KEY:
+        return api_key
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='invaliid api key'
+    )
+
+@app.post("/readings", response_model=ReadingResponse, dependencies=[Depends(get_api_key)])
 async def create_reading(reading: ReadingCreate, db:Session = Depends(get_db)):
     db_reading = SensorReading(**reading.dict())
     db.add(db_reading)
@@ -43,4 +59,4 @@ async def get_readings(limit=100, device_id: str = None, db: Session = Depends(g
     query = db.query(SensorReading) 
     if device_id:
         query = query.filter(SensorReading.device_id == device_id)
-    return query.order_by(SensorReading.timestamp.desc()).limit(limit).all
+    return query.order_by(SensorReading.timestamp.desc()).limit(limit).all()
